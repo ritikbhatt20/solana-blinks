34398e85829af898ce9c85dc020a2c6ec9bddcc0
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-08-27 03:19:17 +0530

    🔨made transaction appearing

--- web/app/api/action/route.ts
+++ web/app/api/action/route.ts
@@ -1,7 +1,9 @@
-import { ActionGetResponse, ActionPostRequest, ActionPostResponse } from '@solana/actions';
+import { ActionGetResponse, ActionPostRequest, ActionPostResponse, ACTIONS_CORS_HEADERS } from '@solana/actions';
+import { PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import { use } from 'react';
 
 export async function GET(request: Request) {
-  const response: ActionGetResponse = {
+  const responseBody: ActionGetResponse = {
     icon: 'https://cdn-img.panewslab.com/panews/2024/07/02/images/2Mcb6mx14J.jpg',
     description: 'This is heyblancos demo blink.',
     title: 'DO BLINK!',
@@ -10,7 +12,9 @@ export async function GET(request: Request) {
       message: 'This blink is not implemented yet!',
     },
   };
-  return Response.json(response);
+
+  const response = Response.json(responseBody, {headers: ACTIONS_CORS_HEADERS});
+  return response;
 }
 
 export async function POST(request: Request) {
@@ -19,10 +23,19 @@ export async function POST(request: Request) {
   const userPubkey = requestBody.account;
   console.log(userPubkey);
 
+  const tx = new Transaction();
+  tx.feePayer = new PublicKey(userPubkey);
+  tx.recentBlockhash = SystemProgram.programId.toBase58();
+  const serialTX = tx.serialize({requireAllSignatures: false, verifySignatures: false}).toString("base64");
+
   const response: ActionPostResponse = {
-    transaction: "",
+    transaction: serialTX,
     message: "hello" + userPubkey
   };
 
-  return Response.json(response)
+  return Response.json(response, {headers: ACTIONS_CORS_HEADERS});
+}
+
+export async function OPTIONS(request: Request) {
+  return new Response(null, {headers: ACTIONS_CORS_HEADERS});
 }
