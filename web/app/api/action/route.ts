b931af17c1de531a023dd9ee9ce26f1f8e28c3d4
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-08-27 03:50:24 +0530

    💲empty transaction successfull

--- web/app/api/action/route.ts
+++ web/app/api/action/route.ts
@@ -1,5 +1,5 @@
 import { ActionGetResponse, ActionPostRequest, ActionPostResponse, ACTIONS_CORS_HEADERS } from '@solana/actions';
-import { PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import { clusterApiUrl, Connection, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
 import { use } from 'react';
 
 export async function GET(request: Request) {
@@ -23,9 +23,10 @@ export async function POST(request: Request) {
   const userPubkey = requestBody.account;
   console.log(userPubkey);
 
+  const connection = new Connection(clusterApiUrl("devnet"));
   const tx = new Transaction();
   tx.feePayer = new PublicKey(userPubkey);
-  tx.recentBlockhash = SystemProgram.programId.toBase58();
+  tx.recentBlockhash = (await connection.getLatestBlockhash({commitment: "finalized"})).blockhash;
   const serialTX = tx.serialize({requireAllSignatures: false, verifySignatures: false}).toString("base64");
 
   const response: ActionPostResponse = {
